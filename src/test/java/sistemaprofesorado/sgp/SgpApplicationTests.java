f356cf447e2de820ca2b420c2b0c04fd9ec6108e
Marcelo Alcántara <46867671+Marcee13@users.noreply.github.com>
2026-01-29 09:16:24 -0600

    Creación del proyecto

---
+++ src/test/java/sistemaprofesorado/sgp/SgpApplicationTests.java
@@ -0,0 +1,13 @@
+package sistemaprofesorado.sgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
